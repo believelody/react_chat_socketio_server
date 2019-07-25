83c86aa1df820f0080368a5c4bd37a04e75b525f
believelody <believelody@gmail.com>
2019-07-25 13:33:48 +0200

    added typing event

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http"),
   express = require("express"),
   SocketIO = require("socket.io"),
   bodyParser = require("body-parser"),
-  cors = require('cors'),
+  cors = require("cors"),
   path = require("path"),
   uuid = require("uuid"),
   app = express();
@@ -15,33 +15,37 @@ const api = require("./routes/api");
 let clients = [];
 let chats = [];
 
-app.use(cors())
+app.use(cors());
 
 io.sockets.on("connection", socket => {
   let id = socket.id;
   // console.log("client connected");
 
-  socket.emit("client-emit", id);
+  // socket.emit("client-emit", id);
 
   socket.on("user-emit", data => {
     if (!clients.find(client => client.username === data.username))
-      clients.push(data);
-
+      clients.push({ ...data, id });
+    console.log(clients);
     io.emit("fetch-users", clients);
   });
 
   socket.on("new-chat", data => {
-    let chat = chats.find(chat => chat.users.filter(user => data.map(d => user.id === d)))
+    let chat = chats.find(chat =>
+      chat.users.filter(user => data.map(d => user.id === d))
+    );
 
     if (!chat) {
-      let users = clients.filter(client => data.map(d => d === client.id))
-      chat = { id: uuid(), users, messages: [] }
-      chats.push(chat)
+      let users = clients.filter(client =>
+        data.map(d => d === client.username)
+      );
+      chat = { id: uuid(), users, messages: [] };
+      chats.push(chat);
     }
-    socket.join(chat.id)
+    socket.join(chat.id);
     socket.emit("fetch-chat", chat);
     // console.log(chat.messages)
-    // socket.to(chat.id).emit('fetch-messages', chat.messages) 
+    // socket.to(chat.id).emit('fetch-messages', chat.messages)
   });
 
   socket.emit("fetch-chats", chats);
@@ -52,7 +56,7 @@ io.sockets.on("connection", socket => {
     let chat = chats.find(chat => chat.id === chatId);
 
     if (chat) {
-      socket.join(chat.id)
+      socket.join(chat.id);
       chat["users"].push(userId);
       socket.to(chat.id).emit("refresh-users-from-chat", chat.users);
     } else {
@@ -60,18 +64,18 @@ io.sockets.on("connection", socket => {
     }
   });
 
-  socket.on("remove-user-from-chat", (userId, chatId) => {
+  socket.on("remove-user-from-chat", (username, chatId) => {
     let chat = chats.find(chat => chat.id === chatId);
 
     if (chat) {
-      chat["users"].filter(user => user.id !== userId);
+      chat["users"].filter(user => user.username !== username);
       socket.emit("refresh-users-from-chat", chat.users);
     } else {
       socket.emit("error-in-chat", "An error occured");
     }
   });
 
-  socket.on("new-message", (message) => {
+  socket.on("new-message", message => {
     let chat = chats.find(chat => chat.id === message.chatId);
 
     if (chat) {
@@ -85,15 +89,27 @@ io.sockets.on("connection", socket => {
 
       io.to(chat.id).emit("fetch-chat", chat);
     } else {
-      console.log('error')
+      console.log("error");
       socket.emit("error-in-chat", "An error occured");
     }
   });
 
+  socket.on("typing", () => {
+    socket.broadcast.emit("is-typing", true);
+  });
+
+  socket.on("stop-typing", () => {
+    socket.broadcast.emit("is-typing", false);
+  });
+
   socket.on("disconnect", () => {
+    clients.filter(client => {
+      if (client.id === id) {
+        socket.broadcast.emit("user-disconnected", client.username);
+        return null;
+      } else return client;
+    });
     console.log(clients);
-    clients.filter(client => client.id !== id);
-    socket.broadcast.emit("user-disconnect", id);
   });
 });
 

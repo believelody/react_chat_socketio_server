d97d6eca3f1071b7c528e57b95e87a34aa353db4
believelody <believelody@gmail.com>
2019-07-25 05:27:06 +0200

    add html file

--- index.js
+++ index.js
@@ -111,8 +111,8 @@ app.use("/api", api);
 //  Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   //  Set static folder
-  app.use(express.static("../client/build"));
+  app.use(express.static("index.html"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
   });
 }

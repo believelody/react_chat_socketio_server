572b4eab29ff69374d82db151dc6515f57e8b737
believelody <believelody@gmail.com>
2019-08-26 20:50:39 +0200

    Change nodemon to node

--- utils/allowCors.js
+++ utils/allowCors.js
@@ -9,7 +9,7 @@ module.exports = app => {
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, OPTIONS"
     );
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
     res.header("Access-Control-Allow-Credentials", true);
     return next();
   });
